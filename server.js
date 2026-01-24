9abb42e3f80251dd261446a082b75ed915326cf0
ahmad_sakour <hussian0938605821@gmail.com>
2026-01-24 16:37:16 +0300

    edite in server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
 
 async function testDbConnection() {
     try {
