d4b584ee0d350d0bb0b05562490aaf4db128462a
ahmad_sakour <hussian0938605821@gmail.com>
2026-01-30 15:50:45 +0300

    update db

--- db.js
+++ db.js
@@ -1,27 +1,13 @@
 const mysql = require('mysql2/promise');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
-    waitForConnections: true,
-    connectionLimit: 10,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
-module.exports = {
-  query: async (sql, params) => {
-    try {
-      const [rows] = await pool.execute(sql, params);
-      return rows;
-    } catch (error) {
-      console.error('Database Query Error:', error);
-      throw error;
-    }
-  },
-  pool
-};
+module.exports = pool;
